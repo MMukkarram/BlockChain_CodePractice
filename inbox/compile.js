ca75e71038378f9da70b0a63b08964c746d8f64a
Muhammad Mukkarram <mukkarram.moin@venturedive.com>
2023-02-15 01:52:24 +0500

    init

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,9 +1,10 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+const path = require('path'); // import path for fetching directory path
+const fs = require('fs');     // import fs for reading file content
+const solc = require('solc');  // import solc for executing sol file.
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf8");
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");  // Fetching Inbox.sol from directory
+const source = fs.readFileSync(inboxPath, "utf8");                    // Read file content and define what the content type is ie utf8
+
 
 /***
  * The recommended way to interface with the Solidity compiler, especially for more
@@ -12,6 +13,7 @@ const source = fs.readFileSync(inboxPath, "utf8");
  * See https://docs.soliditylang.org/en/v0.8.5/using-the-compiler.html#compiler-input-and-output-json-description
  * for more details.
  */
+
 const input = {
   language: "Solidity",
   sources: {
@@ -34,6 +36,21 @@ const input = {
   }
 };
 
-const output = JSON.parse(solc.compile(JSON.stringify(input)));
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-module.exports = output.contracts["Inbox.sol"].Inbox;
+console.log(output);
+
+/*  output is like 
+{ contracts: { 'Inbox.sol': { Inbox: [Object] } },
+  sources: { 'Inbox.sol': { id: 0 } } }
+
+  so we have to sent abi and bytecode separately from the output object 
+  this contracts folder only contain one contrain if it has multiple then multiple output keys will be exported.
+*/
+
+exports.abi = output.contracts['Inbox.sol']['Inbox'].abi;
+exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+
+
+
+// Now use command node compile.js to compile the file
