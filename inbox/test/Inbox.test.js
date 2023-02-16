f9d4327aabf11fd62d350abaf801457429140743
Muhammad Mukkarram <mukkarram.moin@venturedive.com>
2023-02-17 04:30:27 +0500

    Mocha version updated. It is working fine Now ALlahumdullilah

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -4,8 +4,23 @@ const Web3 = require('web3'); // when we declare web3, we used Capital Web3 beca
 const web3 = new Web3(ganache.provider()); // ganache.provider() will change according to the network current we are deploying our contract on local network ganache, so it will change when we live or on rinkybey etc
 
 
+beforeEach(() => {
 
+    //Get a list of all account (local account created by the ganache for testing. these are open account ie without passwords for testing)
+    web3.eth.getAccounts().then(fetchedAcounts => {
+        console.log(fetchedAcounts);
+    });
 
+    //Use one of these account for to deploy.
+});
+
+describe('Inbox Testing', () => {
+    it('Deploys a contract', () => { })
+});
+
+//module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+/*
 // Code for understaing mocha testing
 class Car{
     park(){
@@ -16,13 +31,17 @@ class Car{
     }
 }
 
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
 describe("Car Class Test cases", () => {
 it("park func test case", () => {
-    const car = new Car();
     assert.equal(car.park(),"parked");
 });
 it("onTheWay func test case", () => {
-    const car = new Car();
     assert.equal(car.onTheWay(),"Running");
 });
-});
+});*/
