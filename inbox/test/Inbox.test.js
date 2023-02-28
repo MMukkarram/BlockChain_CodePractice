90e744fee831fc54328d3f8fcc24f8753f8895b4
Muhammad Mukkarram <mukkarram.moin@venturedive.com>
2023-03-01 00:17:31 +0500

    vheck

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -26,6 +26,7 @@ beforeEach(async () => {
      // basically we pass bytecode and the contructor arguments in deploy which is used in Inbox constuctor.
     .send({from: accounts[0], gas:'1000000'}); 
     // we call send method to send the transactions and also mention some amount of gas fees which is required to complete the transaction
+    
 });
 
 describe('Inbox Testing', () => {
